c4429bc9588d1e41e8b2618695a1487afa42af8f
Dany Raskin <danyyyyy223@gmail.com>
2022-05-01 01:47:39 +0300

    Added possible movement

--- Piece.js
+++ Piece.js
@@ -1,5 +1,29 @@
-class Piece{
+class Piece {
     constructor(player) {
         this.player = player;
     }
+}
+function pieceMovement(row, col, color) {
+    if (color === BLACK_PLAYER) {
+        if (isPointInBounds(row + 1, col + 1) && pieces[row + 1][col + 1] === undefined) {
+            paintPossibleMove(row + 1, col + 1);
+        }
+        if (isPointInBounds(row + 1, col - 1) && pieces[row + 1][col - 1] === undefined) {
+            paintPossibleMove(row + 1, col - 1);
+        }
+    }
+    else {
+        if (isPointInBounds(row - 1, col + 1) && pieces[row - 1][col + 1] === undefined) {
+            paintPossibleMove(row - 1, col + 1);
+        }
+        if (isPointInBounds(row - 1, col - 1) && pieces[row - 1][col - 1] === undefined) {
+            paintPossibleMove(row - 1, col - 1);
+        }
+    }
+}
+function paintPossibleMove(row, col) {
+    htmlTable.rows[row].cells[col].classList.add('possibleMove');
+}
+function isPointInBounds(x, y) {
+    return (x >= 0 && y >= 0 && x < TABLE_SIZE && y < TABLE_SIZE);
 }
--- game.js
+++ game.js
@@ -1,13 +1,36 @@
+//global varibales declerations
+let selectedCell;
+let pieces = [];
+let currentPiece;
 const htmlTable = document.createElement('table');
 const TABLE_SIZE = 8;
-let pieces = [];
 const RED_PLAYER = 'red';
 const BLACK_PLAYER = 'black';
 const RED_STARTING_ROW = 5;
 const BLACK_STARTING_ROW = 0;
 
-function onCellClick(e){
+function onCellClick(e) {
 
+    if (selectedCell === undefined && pieces[e.currentTarget.parentNode.rowIndex][e.currentTarget.cellIndex] !== undefined) {
+        selectedCell = e.currentTarget;
+        selectedCell.classList.add('selected');
+        currentPiece = pieces[selectedCell.parentNode.rowIndex][selectedCell.cellIndex];
+        pieceMovement(e.currentTarget.parentNode.rowIndex, e.currentTarget.cellIndex, currentPiece.player);
+    }
+    else if (selectedCell !== undefined) {
+        unpaintAllCells();
+        selectedCell = undefined;
+        onCellClick(e);
+    }
+}
+function unpaintAllCells() {
+    for (let i = 0; i < TABLE_SIZE; i++) {
+        for (let j = 0; j < TABLE_SIZE; j++) {
+            htmlTable.rows[i].cells[j].classList.remove('selected');
+            htmlTable.rows[i].cells[j].classList.remove('possibleMove');
+            htmlTable.rows[i].cells[j].classList.remove('possibleEat');
+        }
+    }
 }
 game = new BoardData();
 window.addEventListener('load', game.initCheckersGame);
