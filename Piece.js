bc15b1ad38015c1e0996a7d4530101dd8a4c25a9
Dany Raskin <danyyyyy223@gmail.com>
2022-05-01 18:27:46 +0300

    Added ability to eat enemy pieces

--- Piece.js
+++ Piece.js
@@ -1,28 +1,50 @@
 class Piece {
-    constructor(player) {
-        this.player = player;
+    constructor(color) {
+        this.color = color;
     }
 }
 function pieceMovement(row, col, color) {
     if (color === BLACK_PLAYER) {
-        if (isPointInBounds(row + 1, col + 1) && pieces[row + 1][col + 1] === undefined) {
+        if (isPointInBounds(row + 1, col + 1) && pieces[row + 1][col + 1] !== undefined && pieces[row + 1][col + 1].color !== color && isPointInBounds(row + 2, col + 2) && pieces[row + 2][col + 2] === undefined) {
+            paintPossibleEat(row + 1, col + 1);
+            paintPossibleAfterEat(row + 2, col + 2);
+        }
+        else if (isPointInBounds(row + 1, col + 1) && pieces[row + 1][col + 1] === undefined) {
             paintPossibleMove(row + 1, col + 1);
         }
-        if (isPointInBounds(row + 1, col - 1) && pieces[row + 1][col - 1] === undefined) {
+        if (isPointInBounds(row + 1, col - 1) && pieces[row + 1][col - 1] !== undefined && pieces[row + 1][col - 1].color !== color && isPointInBounds(row + 2, col - 2) && pieces[row + 2][col - 2] === undefined) {
+            paintPossibleEat(row + 1, col - 1);
+            paintPossibleAfterEat(row + 2, col - 2);
+        }
+        else if (isPointInBounds(row + 1, col - 1) && pieces[row + 1][col - 1] === undefined) {
             paintPossibleMove(row + 1, col - 1);
         }
     }
     else {
-        if (isPointInBounds(row - 1, col + 1) && pieces[row - 1][col + 1] === undefined) {
+        if (isPointInBounds(row - 1, col + 1) && pieces[row - 1][col + 1] !== undefined && pieces[row - 1][col + 1].color !== color && isPointInBounds(row - 2, col + 2) && pieces[row - 2][col + 2] === undefined) {
+            paintPossibleEat(row - 1, col + 1);
+            paintPossibleAfterEat(row - 2, col + 2);
+        }
+        else if (isPointInBounds(row - 1, col + 1) && pieces[row - 1][col + 1] === undefined) {
             paintPossibleMove(row - 1, col + 1);
         }
-        if (isPointInBounds(row - 1, col - 1) && pieces[row - 1][col - 1] === undefined) {
+        if (isPointInBounds(row - 1, col - 1) && pieces[row - 1][col - 1] !== undefined && pieces[row - 1][col - 1].color !== color && isPointInBounds(row - 2, col - 2) && pieces[row - 2][col - 2] === undefined) {
+            paintPossibleEat(row - 1, col - 1);
+            paintPossibleAfterEat(row - 2, col - 2);
+        }
+        else if (isPointInBounds(row - 1, col - 1) && pieces[row - 1][col - 1] === undefined) {
             paintPossibleMove(row - 1, col - 1);
         }
     }
 }
 function paintPossibleMove(row, col) {
     htmlTable.rows[row].cells[col].classList.add('possibleMove');
+}
+function paintPossibleEat(row, col) {
+    htmlTable.rows[row].cells[col].classList.add('possibleEat');
+}
+function paintPossibleAfterEat(row, col) {
+    htmlTable.rows[row].cells[col].classList.add('possibleAfterEat');
 }
 function isPointInBounds(x, y) {
     return (x >= 0 && y >= 0 && x < TABLE_SIZE && y < TABLE_SIZE);
--- game.js
+++ game.js
@@ -15,18 +15,37 @@ function onCellClick(e) {
         selectedCell = e.currentTarget;
         selectedCell.classList.add('selected');
         currentPiece = pieces[selectedCell.parentNode.rowIndex][selectedCell.cellIndex];
-        pieceMovement(e.currentTarget.parentNode.rowIndex, e.currentTarget.cellIndex, currentPiece.player);
+        pieceMovement(e.currentTarget.parentNode.rowIndex, e.currentTarget.cellIndex, currentPiece.color);
     }
     else if (e.currentTarget.classList.contains('possibleMove')) {
         let row = selectedCell.parentNode.rowIndex; //selectedCell contains the cell of the piece from the previous click
         let col = selectedCell.cellIndex;
         pieces[row][col] = undefined;
-        selectedCell.classList.remove('checker-' + currentPiece.player);
+        selectedCell.classList.remove('checker-' + currentPiece.color);
+
         selectedCell = e.currentTarget; //now selectedCell contains the clicked on cell
         row = selectedCell.parentNode.rowIndex;
         col = selectedCell.cellIndex;
         pieces[row][col] = currentPiece;
-        selectedCell.classList.add('checker-' + currentPiece.player);
+        selectedCell.classList.add('checker-' + currentPiece.color);
+        unpaintAllCells();
+    }
+    else if (e.currentTarget.classList.contains('possibleAfterEat')) {
+        let row = selectedCell.parentNode.rowIndex; //selectedCell contains the cell of the piece from the previous click
+        let col = selectedCell.cellIndex;
+        pieces[row][col] = undefined;
+        selectedCell.classList.remove('checker-' + currentPiece.color);
+
+        selectedCell = e.currentTarget; //now selectedCell contains the clicked on cell
+        let newRow = selectedCell.parentNode.rowIndex;
+        let newCol = selectedCell.cellIndex;
+        let x = (newRow - row) - ((newRow - row) / 2); //I'm checking where is the eaten piece located/what is the direction of the eaten piece from the piece that ate it
+        let y = (newCol - col) - ((newCol - col) / 2);
+        let opponentColor = currentPiece.color === BLACK_PLAYER ? RED_PLAYER : BLACK_PLAYER;
+        htmlTable.rows[row + x].cells[col + y].classList.remove('checker-' + opponentColor); //removing the eaten piece by adding the direction (x,y) to the row and column of the piece that ate it
+        selectedCell.classList.add('checker-' + currentPiece.color);
+        pieces[newRow][newCol] = currentPiece;
+        pieces[row + x][col + y] = undefined;
         unpaintAllCells();
     }
     else if (selectedCell !== undefined) {
@@ -41,6 +60,7 @@ function unpaintAllCells() {
             htmlTable.rows[i].cells[j].classList.remove('selected');
             htmlTable.rows[i].cells[j].classList.remove('possibleMove');
             htmlTable.rows[i].cells[j].classList.remove('possibleEat');
+            htmlTable.rows[i].cells[j].classList.remove('possibleAfterEat');
         }
     }
 }
